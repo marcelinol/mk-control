f851cd3a3b678b382797f21e5629990b7164e5a7
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-02-03 21:55:26 -0200

    validate uniqueness of consultant name

--- app/models/consultant.rb
+++ app/models/consultant.rb
@@ -3,4 +3,5 @@ class Consultant < ActiveRecord::Base
   has_many :products
 
   validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- spec/models/consultant_spec.rb
+++ spec/models/consultant_spec.rb
@@ -5,5 +5,12 @@ RSpec.describe Consultant, :type => :model do
     let(:consultant) { Consultant.new(name: nil) }
 
     it { expect(consultant).not_to be_valid }
+
+    it 'does not duplicate name' do
+      create(:consultant)
+      new_consultant = build(:consultant)
+
+      expect(new_consultant).not_to be_valid
+    end
   end
 end
