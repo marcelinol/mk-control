72c5d03177c358394ecb8b7f7bbf2fafd229a180
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-02-03 22:35:16 -0200

    mark product as sold when sold

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,11 +5,19 @@ class Product < ActiveRecord::Base
 
   validates :name, :consultant, :request, presence: true
 
+  after_save :handle_selling, if: 'sale_id_changed?'
+
   enum status: [:stock, :sold, :sample, :to_be_defined]
 
   scope :in_stock,      -> { where(status: 0) }
   scope :sold,          -> { where(status: 1) }
   scope :sample,        -> { where(status: 2) }
   scope :to_be_defined, -> { where(status: 3) }
+
+  private
+
+  def handle_selling
+    sold! if sale_id_was.nil? && !sold?
+  end
 
 end
