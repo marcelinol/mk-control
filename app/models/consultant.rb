8562841a655026c7196a06ee8f3b396244f64a56
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-02-21 07:51:22 -0300

    updates balance when income or outcome has changed

--- app/models/consultant.rb
+++ app/models/consultant.rb
@@ -4,4 +4,16 @@ class Consultant < ActiveRecord::Base
   has_many :customers
 
   validates :name, presence: true
+
+  after_save :update_balance, if: :must_update_balance?
+
+  private
+
+  def update_balance
+    self.balance = self.income - self.outcome
+  end
+
+  def must_update_balance?
+    self.outcome_changed? || self.income_changed?
+  end
 end
--- spec/models/consultant_spec.rb
+++ spec/models/consultant_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe Consultant, :type => :model do
 
     it { expect(consultant).not_to be_valid }
   end
+
+  context 'callbacks' do
+    let(:consultant) { create(:consultant) }
+
+    it 'updates balance' do
+      expect(consultant.balance).to eq 0
+
+      consultant.income = BigDecimal.new(100)
+      consultant.save
+      expect(consultant.balance).to be > 0
+
+      consultant.outcome = BigDecimal.new(300)
+      consultant.save
+      expect(consultant.balance).to be < 0
+    end
+  end
 end
