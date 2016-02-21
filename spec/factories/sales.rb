45ae9c83a63410df6c50669b711121419609cb98
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-02-21 08:23:04 -0300

    create sale_with_product factory

--- spec/factories/sales.rb
+++ spec/factories/sales.rb
@@ -2,4 +2,11 @@ FactoryGirl.define do
   factory :sale do
     association :customer, factory: :customer
   end
+
+  factory :sale_with_product, class: Sale do
+    association :customer, factory: :customer
+    before(:create) do |sale|
+      FactoryGirl.create(:product, sale: sale)
+    end
+  end
 end
