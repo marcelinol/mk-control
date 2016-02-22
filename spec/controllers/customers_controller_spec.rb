aedf1d31752706272d609c7c555da02e2259f0bd
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-02-21 22:22:54 -0300

    Sign in user in controller specs which were breaking

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe CustomersController, :type => :controller do
 
+  before do
+    @user = FactoryGirl.create(:user)
+    sign_in @user
+  end
+
   describe 'GET index' do
     it 'returns http success' do
       get :index
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    email    'dev@example.org'
-    password '123qwe'
+    email                 'dev@example.org'
+    password              '123qwe'
+    password_confirmation '123qwe'
   end
 end
