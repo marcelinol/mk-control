823bc3e325fa641319e97d4820dd3b687d957cf2
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-03-28 19:15:45 -0300

    Avoid warning

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,6 @@ module MkControl
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = 'pt-BR'
-    config.serve_static_assets = true
+    config.serve_static_files = true
   end
 end
