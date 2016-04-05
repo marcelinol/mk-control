73d9293161d89095325ccc1d20d5a8a47756aecf
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-04-05 20:54:52 -0300

    log sidekiq

--- app/workers/reminder_worker.rb
+++ app/workers/reminder_worker.rb
@@ -5,6 +5,9 @@ class ReminderWorker
   def perform(consultant_id, customer_id)
     consultant = Consultant.find(consultant_id)
     customer = Customer.find(customer_id)
+    logger.info ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    logger.info "I got consultant #{consultant.name} with id #{consultant.id}"
+    logger.info "I got customer #{customer.name} with id #{customer.id}"
     ReminderMailer.two_days_contact_reminder(consultant, customer).deliver
   end
 
