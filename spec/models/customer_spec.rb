23ba0be1d2fc610daf2adcbf7eb24043f282a6aa
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-04-05 20:54:42 -0300

    i dont remember too

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -27,4 +27,14 @@ RSpec.describe Customer, :type => :model do
     end
   end
 
+  context 'scopes' do
+    let!(:consultant) { create(:consultant) }
+    let!(:customer)   { create(:customer, next_contact_date: Time.now) }
+
+    before { consultant.customers << customer  }
+
+    it { binding.pry; expect(consultant.customers.must_contact_today.count).to eq 1 }
+
+  end
+
 end
