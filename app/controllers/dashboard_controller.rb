fb62ea9b049342d757cfa2c23b00521faaca2723
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-04-10 18:53:22 -0300

    Dashboard refactor - Removed financial part which was not working completely - Added phone number to consultants to talk today

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
     @consultant = current_user.consultant
-    @customers_for_today = Customer.must_contact_today.where(consultant: @consultant).pluck(:name)
+    @customers_for_today = Customer.must_contact_today.where(consultant: @consultant)
   end
 end
